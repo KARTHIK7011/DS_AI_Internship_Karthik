4304f5e3314d2ff1424197628c5478d88041d9c8
KARTHIK7011 <karthikpoojary6926@gmail.com>
2026-02-09 14:41:22 +0530

    Add files via upload

---
+++ src/DAY6/task2.py
@@ -0,0 +1,8 @@
+
+
+def power(base, exp):
+    return base ** exp
+
+
+def average(numbers_list):
+    return sum(numbers_list) / len(numbers_list)
---
+++ src/DAY6/task3.py
@@ -0,0 +1,12 @@
+
+
+import task2
+
+
+result_power = task2.power(2, 10)
+
+numbers = [10, 20, 30, 40]
+result_average = task2.average(numbers)
+
+print("2^10 =", result_power)
+print("Average =", result_average)
